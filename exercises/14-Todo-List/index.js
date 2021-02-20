a296b4fe09b7bb69d0a704b39f40bd8d9bd7298d
johmstone <johmstone@gmail.com>
2021-02-20 03:07:47 +0000

    Solution exercise 14

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,33 @@
 // Your code here
+let x = document.getElementById("addToDo");
+let y = document.querySelector("ul");
+let w = document.getElementsByClassName("fa-trash");
+
+x.addEventListener("keyup", e => {
+	if (e.key === "Enter") {
+		console.log(x.value);
+		let content = `<li>
+					        <span><i class="fa fa-trash"></i></span> ${x.value}
+				        </li>`;
+		//li.textContent = content;
+		y.innerHTML += content;
+		cleanInput();
+	}
+});
+
+function cleanInput() {
+	x.value = "";
+}
+
+y.addEventListener("click", e => {
+	const targetElem = e.target.parentNode;
+
+	if (targetElem.nodeName === "SPAN") {
+		//console.log(targetElem.parentNode);
+		y.removeChild(targetElem.parentNode);
+	} else {
+		//console.log(targetElem);
+		y.removeChild(targetElem);
+	}
+	//targetElem.parentNode.removeChild(targetElem);
+});
